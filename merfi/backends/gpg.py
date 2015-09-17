43e9538c5379eb9125f6c6093dff0e99b8898626
Alfredo Deza <adeza@redhat.com>
2015-09-17 12:05:57 -0400

    need to pass --yes to gpg to overwrite Release
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- merfi/backends/gpg.py
+++ merfi/backends/gpg.py
@@ -4,6 +4,7 @@ from merfi.collector import FileCollector
 import merfi
 from merfi import logger
 from merfi import util
+from merfi.backends import BaseBackend
 
 
 class Gpg(object):
@@ -31,17 +32,8 @@ Positional Arguments:
 
     def __init__(self, argv):
         self.argv = argv
+        # XXX not sure why/if we need this
         self.default_keyfile = '/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release'
-
-    def get_path(self, arguments):
-        try:
-            path = arguments[-1]
-        except IndexError:
-            path = '.'
-        if os.path.exists(path):
-            return os.path.abspath(path)
-        else:
-            return os.path.abspath('.')
 
     def parse_args(self):
         parser = Transport(self.argv)
@@ -49,13 +41,8 @@ Positional Arguments:
         parser.parse_args()
         file_output = parser.get('--output') or self.default_keyfile
         merfi.config['path'] = self.get_path(parser.arguments)
-        self.check_dependencies()
+        util.check_dependency(self.executable)
         self.sign(file_output)
-
-    def check_dependencies(self):
-        if not util.which(self.executable):
-            logger.error('could not find %s' % self.executable)
-            raise RuntimeError('%s needs to be installed and available in $PATH' % self.executable)
 
     def sign(self, file_output):
         logger.info('Starting path collection, looking for files to sign')
@@ -64,8 +51,8 @@ Positional Arguments:
             logger.info('%s matching paths found' % len(paths))
             # FIXME: this should spit the actual verified command
             logger.info('will sign with the following commands:')
-            logger.info('gpg --armor --detach-sig --output Release.gpg Release')
-            logger.info('gpg --clearsign --output InRelease Release')
+            logger.info('gpg --batch --yes --armor --detach-sig --output Release.gpg Release')
+            logger.info('gpg --batch --yes --clearsign --output InRelease Release')
         else:
             logger.warning('No paths found that matched')
 
@@ -79,8 +66,8 @@ Positional Arguments:
             else:
                 os.chdir(os.path.dirname(path))
                 # FIXME: this needs to allow for configurable output name
-                detached = ['gpg', '--armor', '--detach-sig', '--output', 'Release.gpg', 'Release']
-                clearsign = ['gpg', '--clearsign', '--output', 'InRelease', 'Release']
+                detached = ['gpg', '--batch', '--yes', '--armor', '--detach-sig', '--output', 'Release.gpg', 'Release']
+                clearsign = ['gpg', '--batch', '--yes','--clearsign', '--output', 'InRelease', 'Release']
                 logger.info('signing: %s' % path)
                 util.run(detached)
                 util.run(clearsign)
