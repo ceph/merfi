d7a9af6ebfa82cec80a6aa6b9694d4765291a084
Alfredo Deza <adeza@redhat.com>
2015-09-24 13:19:25 -0400

    add a bunch of tests for the collector
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- merfi/tests/test_filecollector.py
+++ merfi/tests/test_filecollector.py
@@ -2,7 +2,11 @@ import os
 import pytest
 from merfi.collector import FileCollector
 
+
 class TestFileCollector(object):
+
+    def setup(self):
+        self.paths = FileCollector({'path': '/'}, _eager=False)
 
     def test_simple_tree(self, repotree):
         paths = FileCollector({'path': repotree})
@@ -11,3 +15,21 @@ class TestFileCollector(object):
             os.path.join(repotree, 'dists', 'trusty',  'Release'),
         ]
         assert set(paths) == set(expected)
+
+    def test_path_is_absolute(self):
+        assert self.paths._abspath('/') == '/'
+
+    def test_path_is_not_absolute(self):
+        assert self.paths._abspath('directory').startswith('/')
+
+    def test_valid_name_fallsback(self):
+        assert self.paths.valid_name.pattern == 'Release$'
+
+    def test_valid_name_usermatch(self):
+        self.paths.user_match = 'myfile'
+        assert self.paths.valid_name.pattern == 'myfile'
+
+    def test_invalid_regex(self):
+        self.paths.user_match = 1
+        with pytest.raises(SystemExit):
+            self.paths.valid_name
