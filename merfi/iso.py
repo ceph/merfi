8fabfe4a68f29812e7a0e91be824a40d71abd9f8
Alfredo Deza <adeza@redhat.com>
2015-09-24 18:06:41 -0400

    create an ISO handler
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ merfi/iso.py
@@ -0,0 +1,33 @@
+import os
+from tambo import Transport
+from merfi.collector import FileCollector
+import merfi
+from merfi import logger
+from merfi import util
+
+
+class Iso(object):
+    help_menu = 'simple ISO manipulation'
+    _help = """
+A very simple interface to create ISOs with some sane defaults (like ensure
+source directory has proper read permissions).
+
+Default behavior will perform these actions on a source directory::
+
+    genisoimage -r -o isofile {source directory}
+
+Options:
+
+--output      Custom filename output (defaults to 'isofile').
+
+Positional Arguments:
+
+[source directory]  The path to create the ISO from. Required with no defaults.
+"""
+    executable = 'genisoimage'
+
+    def __init__(self, argv):
+        self.argv = argv
+
+    def parse_args(self):
+        pass
