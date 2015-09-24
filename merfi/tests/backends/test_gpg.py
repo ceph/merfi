886ce135d03a24a03fd7d4ed94be69a9042db167
Ken Dreyer <kdreyer@redhat.com>
2015-09-24 09:31:42 -0600

    tests: test Gpg class
    Verify that merfi is calling util.run() with the right parameters. This
    test uses a basic BaseBackendTest parent class that we can eventually
    use to test the RpmSign backend as well.

--- merfi/tests/backends/test_gpg.py
+++ merfi/tests/backends/test_gpg.py
@@ -1,5 +1,20 @@
+from mock import patch
+import pytest
 from merfi.backends import gpg
+from merfi.tests.backends.base import BaseBackendTest
 
+class TestGpg(BaseBackendTest):
 
-class TestGpgGetPath(object):
-    pass
+    backend = gpg.Gpg([])
+
+    # args to merfi.backends.gpg.util's run()
+    detached = ['gpg', '--batch', '--yes', '--armor', '--detach-sig', '--output', 'Release.gpg', 'Release']
+    clearsign = ['gpg', '--batch', '--yes', '--clearsign', '--output', 'InRelease', 'Release']
+
+    @patch("merfi.backends.gpg.util")
+    def test_sign_no_files(self, m_util):
+        return super(TestGpg, self).test_sign_no_files(m_util)
+
+    @patch("merfi.backends.gpg.util")
+    def test_sign_two_files(self, m_util, repotree):
+        return super(TestGpg, self).test_sign_two_files(m_util, repotree)
