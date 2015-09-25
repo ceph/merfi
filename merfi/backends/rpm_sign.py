503a3b5bb416cb9a91b7531eaa5c657f1c0181f3
Alfredo Deza <adeza@redhat.com>
2015-09-25 10:21:43 -0400

    make RpmSign use the new base backend class
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- merfi/backends/rpm_sign.py
+++ merfi/backends/rpm_sign.py
@@ -4,13 +4,18 @@ import merfi
 from merfi import logger
 from merfi import util
 from merfi.collector import FileCollector
+from merfi.backends import base
 
 
-class RpmSign(object):
+class RpmSign(base.BaseBackend):
     help_menu = 'rpm-sign handler for signing files'
     _help = """
 Signs files with rpm-sign. Crawls a given path looking for 'Release' files (by
 default)
+
+%s
+
+Options
 
 --key         Name of the key to use
 --keyfile     Full path location of the keyfile, defaults to
@@ -23,36 +28,14 @@ Positional Arguments:
     """
     executable = 'rpm-sign'
     name = 'rpm-sign'
-
-    def __init__(self, argv):
-        self.argv = argv
-        self.default_keyfile = '/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release'
-
-    def get_path(self, arguments):
-        if os.path.exists(arguments[-1]):
-            return os.path.abspath(arguments[-1])
-        else:
-            return os.path.abspath('.')
-
-    def check_dependencies(self):
-        if not util.which(self.executable):
-            logger.error('could not find %s' % self.executable)
-            raise RuntimeError('%s needs to be installed and available in $PATH' % self.executable)
-
-    def parse_args(self):
-        options = ['--key']
-        parser = Transport(self.argv, options=options)
-        parser.catch_help = self._help
-        parser.parse_args()
-        self.keyfile = parser.get('--keyfile') or self.default_keyfile
-        self.key = parser.get('--key')
-        merfi.config['path'] = self.get_path(parser.arguments)
-        self.check_dependencies()
-        self.sign()
+    options = ['--key']
 
     def sign(self):
         logger.info('Starting path collection, looking for files to sign')
-        paths = FileCollector(merfi.config)
+        self.keyfile = self.parser.get('--keyfile', 'Release.gpg')
+        self.key = self.parser.get('--key')
+        paths = FileCollector(self.path)
+
         if paths:
             logger.info('%s matching paths found' % len(paths))
             # FIXME: this should spit the actual verified command
