71412929c1ffc5b1ef3f08d280c2e2fe6c0c43ae
Ken Dreyer <kdreyer@redhat.com>
2015-10-13 10:12:56 -0600

    tests: refactor assertions out of BaseBackendTest
    The Gpg backend only calls util.run(), whereas the RpmSign backend calls
    both util.run() and util.run_output(). In order to support testing both
    backends using BaseBackendTest, move Gpg's assertions into the TestGpg
    class.

--- merfi/tests/backends/base.py
+++ merfi/tests/backends/base.py
@@ -1,6 +1,5 @@
 import pytest
 import merfi
-from mock import call
 
 # inherit this class when testing a backend
 class BaseBackendTest(object):
@@ -8,7 +7,6 @@ class BaseBackendTest(object):
     def test_sign_no_files(self, m_util):
         self.backend.path = ''
         self.backend.sign()
-        assert not m_util.run.called
 
     def test_sign_two_files(self, m_util, repotree):
         # Use our tempdir fixture.
@@ -18,12 +16,3 @@ class BaseBackendTest(object):
         # the cmdline?)
         merfi.config['check'] = False
         self.backend.sign()
-        # Our repotree fixture has two "Release" files.
-        # Each one gets detached-signed and clearsign'd.
-        calls = [
-            call(self.detached),
-            call(self.clearsign),
-            call(self.detached),
-            call(self.clearsign),
-        ]
-        m_util.run.assert_has_calls(calls)
--- merfi/tests/backends/test_gpg.py
+++ merfi/tests/backends/test_gpg.py
@@ -1,4 +1,4 @@
-from mock import patch
+from mock import call, patch
 import pytest
 from merfi.backends import gpg
 from merfi.tests.backends.base import BaseBackendTest
@@ -13,8 +13,18 @@ class TestGpg(BaseBackendTest):
 
     @patch("merfi.backends.gpg.util")
     def test_sign_no_files(self, m_util):
-        return super(TestGpg, self).test_sign_no_files(m_util)
+        super(TestGpg, self).test_sign_no_files(m_util)
+        assert not m_util.run.called
 
     @patch("merfi.backends.gpg.util")
     def test_sign_two_files(self, m_util, repotree):
-        return super(TestGpg, self).test_sign_two_files(m_util, repotree)
+        super(TestGpg, self).test_sign_two_files(m_util, repotree)
+        # Our repotree fixture has two "Release" files.
+        # Each one gets detached-signed and clearsign'd.
+        calls = [
+            call(self.detached),
+            call(self.clearsign),
+            call(self.detached),
+            call(self.clearsign),
+        ]
+        m_util.run.assert_has_calls(calls)
