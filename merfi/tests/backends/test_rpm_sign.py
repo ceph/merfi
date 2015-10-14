cb26e49423a7b2dbad8e50450f47ead54e399191
Alfredo Deza <adeza@redhat.com>
2015-10-14 09:25:12 -0400

    smaller tests, no super() needed
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- merfi/tests/backends/test_rpm_sign.py
+++ merfi/tests/backends/test_rpm_sign.py
@@ -1,41 +1,57 @@
 from mock import call, patch
 from merfi.backends import rpm_sign
 from merfi.tests.backends.base import BaseBackendTest
+from merfi.tests.util import CallRecorder
 from tambo import Transport
 
 
-class TestRpmSign(BaseBackendTest):
+class RpmSign(object):
 
     def setup(self):
         self.backend = rpm_sign.RpmSign([])
+        self.backend.detached = CallRecorder()
+        self.backend.clear_sign = CallRecorder()
         # fake command-line args
         self.argv = ['merfi', '--key', 'mykey']
-        self.backend.parser = Transport(self.argv, options=self.backend.options)
+        self.backend.parser = Transport(
+            self.argv, options=self.backend.options
+        )
         self.backend.parser.parse_args()
 
         # args to merfi.backends.rpm_sign.util's run()
-        self.detached = ['rpm-sign', '--key', 'mykey', '--detachsign', 'Release', '--output', 'Release.gpg']
+        self.detached = [
+            'rpm-sign', '--key', 'mykey', '--detachsign',
+            'Release', '--output', 'Release.gpg']
         # args to merfi.backends.rpm_sign.util's run_output()
-        self.clearsign = ['rpm-sign', '--key', 'mykey', '--clearsign', 'Release']
+        self.clearsign = [
+            'rpm-sign', '--key', 'mykey',
+            '--clearsign', 'Release']
 
-    @patch('merfi.backends.rpm_sign.util')
-    def test_sign_no_files(self, m_util, tmpdir):
-        super(TestRpmSign, self).test_sign_no_files(m_util, tmpdir)
-        assert not m_util.run.called
-        assert not m_util.run_output.called
+    def sign(self, directory):
+        self.backend.path = directory
+        self.backend.sign()
 
-    @patch('merfi.backends.rpm_sign.util')
-    def test_sign_two_files(self, m_util, repotree):
-        # Fake the return values for inline-signing
-        m_util.run_output.return_value = ('--PGP SIGNED MESSAGE--', None, None)
-        super(TestRpmSign, self).test_sign_two_files(m_util, repotree)
-        detached_calls = [
-            call(self.detached),
-            call(self.detached),
-        ]
-        clearsign_calls = [
-            call(self.clearsign),
-            call(self.clearsign),
-        ]
-        m_util.run.assert_has_calls(detached_calls)
-        m_util.run_output.assert_has_calls(clearsign_calls)
+
+class TestRpmSign(RpmSign):
+
+    def test_sign_no_files(self, tmpdir):
+        self.sign(str(tmpdir))
+        assert self.backend.detached.calls == []
+        assert self.backend.clear_sign.calls == []
+
+    def test_sign_two_files_detached_and_clearsign(self, repotree):
+        self.sign(repotree)
+        assert len(self.backend.detached.calls) == 2
+        assert len(self.backend.clear_sign.calls) == 2
+
+    def test_sign_two_files_detached_args(self, repotree):
+        self.sign(repotree)
+        assert self.backend.detached.calls[0][0][0] == self.detached
+        assert self.backend.detached.calls[1][0][0] == self.detached
+
+    def test_sign_two_files_clear_sign_args(self, repotree):
+        self.sign(repotree)
+        # first call, second argument (the command to run)
+        assert self.backend.clear_sign.calls[0][0][1] == self.clearsign
+        # second call, second argument (the command to run)
+        assert self.backend.clear_sign.calls[1][0][1] == self.clearsign
