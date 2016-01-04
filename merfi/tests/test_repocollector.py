c0293cb180f0222e858ecf2611d41085caa99568
Ken Dreyer <kdreyer@redhat.com>
2016-01-04 15:30:36 -0500

    tests: rename repotree fixture for debian
    ... to make room for an eventual rpm repotree fixture

--- merfi/tests/test_repocollector.py
+++ merfi/tests/test_repocollector.py
@@ -7,10 +7,10 @@ class TestRepoCollector(object):
     def setup(self):
         self.paths = RepoCollector(path='/', _eager=False)
 
-    def test_simple_tree(self, repotree):
-        paths = RepoCollector(path=repotree)
-        # The root of the repotree fixture is itself a repository.
-        expected = [ repotree ]
+    def test_simple_tree(self, deb_repotree):
+        paths = RepoCollector(path=deb_repotree)
+        # The root of the deb_repotree fixture is itself a repository.
+        expected = [ deb_repotree ]
         assert set(paths) == set(expected)
 
     def test_path_is_absolute(self):
@@ -19,23 +19,23 @@ class TestRepoCollector(object):
     def test_path_is_not_absolute(self):
         assert self.paths._abspath('directory').startswith('/')
 
-    def test_debian_release_files(self, repotree):
-        paths = RepoCollector(repotree)
+    def test_debian_release_files(self, deb_repotree):
+        paths = RepoCollector(deb_repotree)
         release_files = paths.debian_release_files
-        # The root of the repotree fixture is itself a repository.
+        # The root of the deb_repotree fixture is itself a repository.
         expected = [
-            join(repotree, 'dists', 'trusty', 'Release'),
-            join(repotree, 'dists', 'precise', 'Release'),
+            join(deb_repotree, 'dists', 'trusty', 'Release'),
+            join(deb_repotree, 'dists', 'precise', 'Release'),
         ]
         assert set(release_files) == set(expected)
 
-    def test_debian_nested_release_files(self, nested_repotree):
+    def test_debian_nested_release_files(self, nested_deb_repotree):
         # go one level up
-        path = dirname(nested_repotree)
+        path = dirname(nested_deb_repotree)
         paths = RepoCollector(path)
         release_files = paths.debian_release_files
         expected = [
-            join(nested_repotree, 'dists', 'trusty', 'Release'),
-            join(nested_repotree, 'dists', 'precise', 'Release'),
+            join(nested_deb_repotree, 'dists', 'trusty', 'Release'),
+            join(nested_deb_repotree, 'dists', 'precise', 'Release'),
         ]
         assert set(release_files) == set(expected)
